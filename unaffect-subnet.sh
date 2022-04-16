82cf46dad9ed35a229f2ca50cd3545162a173f9a
jpbarto <jason.p.barto@gmail.com>
2022-04-16 20:57:15 +0100

    added subnet tagging so that the user doesnt need to specify the original route table

--- unaffect-subnet.sh
+++ unaffect-subnet.sh
@@ -7,13 +7,14 @@ set -e
 ###
 
 SUBNET=$1
-ORIG_ROUTE_TABLE_ID=$2
 echo Preparing to reset the route table for $SUBNET...
 
 REGION=${AWS_DEFAULT_REGION:-us-east-1}
 AZ=$(aws ec2 describe-subnets --subnet-ids $SUBNET --query 'Subnets[].AvailabilityZone' --output text --region $REGION)
 VPC_ID=$(aws ec2 describe-subnets --subnet-ids $SUBNET --query 'Subnets[].VpcId' --output text --region $REGION)
 VPC_CIDR=$(aws ec2 describe-vpcs --vpc-ids $VPC_ID --query 'Vpcs[].CidrBlock' --output text --region $REGION)  
+
+ORIG_ROUTE_TABLE_ID=$(aws ec2 describe-tags --filters Name=resource-id,Values=$SUBNET Name=key,Values=rollback-route-table --query 'Tags[0].Value' --output text --region $REGION)
 
 # replace target subnet route table with new route table
 # get association id for subnet
@@ -29,6 +30,8 @@ echo Restoring the route table $ORIG_ROUTE_TABLE_ID to $SUBNET...
 aws ec2 disassociate-route-table --association-id $ASSOC_ID --region $REGION
 # associate new route table
 aws ec2 associate-route-table --subnet-id $SUBNET --route-table-id $ORIG_ROUTE_TABLE_ID --query 'AssociationState' --region $REGION
+# clear the tag set previously
+aws ec2 delete-tags --resources $SUBNET --tags Key=rollback-route-table,Value=$ORIG_ROUTE_TABLE_ID --region $REGION
 
 echo Deleting the old route table $GW_ROUTE_TABLE_ID...
 # delete the old route table
